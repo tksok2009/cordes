cef4b6490ff87899669fe8a324004ee3fa19c41c
tksok2009 <tksok2009@gmail.com>
2016-08-26 19:00:28 +0900

    totyuu

--- kettei2.php
+++ kettei2.php
@@ -14,8 +14,7 @@ for($i5=0;$i5<=1;$i5++){
 //for($i50=0;$i50<=1;$i50++){
 //for($i100=0;$i100<=1;$i100++){
 //for($i500=0;$i500<=1;$i500++){
-	//$pattern_arary[] = ['500en'=> $i500,'100en'=>$i100,'50en'=>$i50,'10en'=>$i10,'5en'=>$i5,'1en'=>$i1];
-	$pattern_arary[] = ['5en'=>$i5,'1en'=>$i1];
+	$pattern_arary[] = ['500en'=> $i500,'100en'=>$i100,'50en'=>$i50,'10en'=>$i10,'5en'=>$i5,'1en'=>$i1];
 //}
 //}
 //}
@@ -27,7 +26,35 @@ for($i5=0;$i5<=1;$i5++){
 
 //logic
 $check = 0;
+
 foreach ($pattern_arary as $array) {
+
+		//$sum = $entype['5en'] * 5 + $entype['1en'] * 1 ;
+		SumCheck($array,$check,$input_payments);
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+/**
+*
+* @return Int payout
+*
+*/
+function SumCheck($array,&$check,&$input_payments){
 
 $sum = 0;
 
@@ -59,57 +86,42 @@ $sum = 0;
 		}
 		*/
 
-
-
-
-
-		
+		/*
 		if($ck < 1){
 		$sum += $array['5en'] * 5;
 		$ck = PaymentsCheck($sum,$check,$input_payments);
 		}
+		*/
+		
 
-
-
-
-		if($ck < 2){
+		if($ck < 1){
 		$sum += $array['1en'] * 1;
-		$ck = PaymentsCheck($sum,$check,$input_payments,1);
-		}		
-
-
+		$ck = PaymentsCheck($sum,$check,$input_payments);
+		}
 
 		
-		if($sum == 0){
-		$ck = 2;
-		}
+		//if($ck == 3){
+		//$ck = 3;
+		//}
 		
 		//echo $ck;
 
-	} while ($ck <= 1);
+	} while ($ck <= 2);
 
 
-} //end foreach
-
-
+}
 
 
 
-function PaymentsCheck($sum,&$check,&$input_payments,$piriod = 0){
+function PaymentsCheck($sum,&$check,&$input_payments){
 
-//echo $sum;
-
+echo $sum;
 
 	if($sum == $input_payments){
 
 		$check = $check + 1;
-
-		if($piriod = 0){
-		return 1;
-		}else{
-		return 2;
-		}
-
+		echo $check;
+		return 3;
 
 	} elseif($sum < $input_payments){
 
@@ -121,7 +133,6 @@ function PaymentsCheck($sum,&$check,&$input_payments,$piriod = 0){
 
 	}
 		$check = $check + 1;
-
 }
 
 
