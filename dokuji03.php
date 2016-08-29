39c5961e1d21ffe28a1fca3d23e3369d062f22b5
toysking <tksok2009@gmail.com>
2016-08-29 21:04:23 +0900

    90made

--- dokuji03.php
+++ dokuji03.php
@@ -2,37 +2,84 @@
 //
 $input_payments = trim(fgets(STDIN));
 //$n = trim(fgets(STDIN));
-
-//20
-//
 $r = 0;
 
-
+// 20 /5 = 4
 $n = floor($input_payments / 5);
 
 
-for ($i=0; $i < $n ; $i=$i+2) {
 
-	fn($n-$i,$r);
+for ($i=0; $i <= $n ; $i=$i+2) {
+
+$ck = $n-$i; // 4 - 0 = 0/ 4 -2 = 2 / 4-4 = 0 
+
+//print_r($ck . " ");
+
+//if($ck == 2){ $r = $r + 1; }
+
+fn($ck,$r);
 
 }
+
 
 
 
 function fn($n=0,&$r=null){
 
-if($n==0){
-$r = $r + 1;
-return 0;}
+if($n== -1){
+return 0;
+}else{
 
+$r = $r + 1;		
+print_r($n . " ");
 
-$r = $r + $n;		
 		$n = $n-1;
 
-print_r($r);
 
 fn($n,$r);
 }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+if($input_payments>=50){
+// 10 -> 50 logic
+$n = floor(($input_payments - 50)/5);
+
+
+
+for ($i=0; $i <= $n ; $i=$i+2) {
+
+$ck = $n-$i; // 4 - 0 = 0/ 4 -2 = 2 / 4-4 = 0 
+
+//print_r($ck . " ");
+
+//if($ck == 2){ $r = $r + 1; }
+
+fn($ck,$r);
+
+}
+}
+
+
+
+
+
+
+
+
+
+// ---------------------
 
 
 
